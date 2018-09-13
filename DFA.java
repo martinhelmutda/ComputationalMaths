8e300b24520977aa22962cbd52e04de715327d50
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-09-13 16:00:43 -0500

    Complete, with some debugging test

--- DFA.java
+++ DFA.java
@@ -1,13 +1,18 @@
-import Java.lang.*;
+
 
 public class DFA {
   public static void main(String[] args){
 	  
-	  String pattern = "abba";
-	  String alphabet = "ab";
-	  String cadena = "aaaaabbbb";
+//	  String pattern = "abba";
+//	  String alphabet = "ab";
+//	  String cadena = "baabbabbaaba";
 	  
-	  transition(pattern, alphabet);
+	  String pattern = "zyx";
+	  String alphabet = "xyz";
+	  String cadena = "zyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyx";
+	  
+	  System.out.println(finite_automaton_matcher(cadena, transition(pattern, alphabet), pattern.length(), alphabet));
+	  //transition(pattern, alphabet);
 	  
 	  if(pattern.contains(alphabet)) {
 		  System.out.println("Sí está");
@@ -19,9 +24,8 @@ public class DFA {
   }
   
   
-  public static char[][] transition(String pattern,String alphabet){
+  public static int[][] transition(String pattern,String alphabet){
 	  	
-			System.out.println("Sí está");
 			int k, cnum;
 			
 			int m = pattern.length();
@@ -30,39 +34,67 @@ public class DFA {
 			
 			String pq = null;
 			String pk= pattern.substring(0,1);
-			//String pk="abba";
-			//System.out.println(pk);
 			
 			for (int q = 0; q <= m; q++) {
-				//System.out.println(q);
 				cnum=0;
 				
 				
 				for(char c: alphabet.toCharArray()) {
 					k=Math.min(m+1, q+2);
-					//System.out.println(c);
 					pq=pattern.substring(0, q)+c;
 					
 					
 					 do {
 						if(1<=k &&  k<= m+1) {
 							k=k-1;
-							//System.out.print(" "+ k);
 							pk=pattern.substring(0,k);
 						}						
-						//System.out.println(c + " "+pq +" "+pk);
 					} while (!pq.endsWith(pk));
 					 delta[q][cnum]=k;
 					 cnum++;
 				}				
 			}
 	
-			for (int i = 0; i < m+1; i++) {
-				for (int j = 0; j < alphabet.length(); j++) {
-					System.out.print(delta[i][j] +" ");
-				}
-				System.out.println();
-			}
-	  return null;	  
+//			for (int i = 0; i < m+1; i++) {
+//				for (int j = 0; j < alphabet.length(); j++) {
+//					System.out.print(delta[i][j] +" ");
+//				}
+//				System.out.println();
+//			}
+			
+	  return delta;	  
   }
+  
+  public static int  finite_automaton_matcher(String cadena, int [][]matrix, int m, String pattern){
+	  int n = cadena.length();
+	  int q = 0, aux, contador=0;
+	  char [] cadenaArr= cadena.toCharArray();
+	  for (int i = 0; i < n; i++) {		
+		  
+		aux=numChar(cadenaArr[i],pattern);
+//		System.out.println("HOLA:" + cadenaArr[i]+" so "+ aux);
+		
+		q = matrix[q][aux];
+		
+//		System.out.println("Esta es i:"+q+"  "+aux + " " + q);
+		if (q==m) {
+//			System.out.println("Pattern occurs" + q);
+			contador++;
+		}
+	  }
+  return contador;
+  }
+  
+  public static int numChar(char x, String alphabet) {
+	  char [] alphabetArr = alphabet.toCharArray();
+	  int num=0;
+	  for (int i= 0; i < alphabetArr.length;i++) {
+//		  System.out.println(x +" estamos en este numero " +alphabetArr[i]);
+		   if(alphabetArr[i]==x) {
+			   num=i;
+		   }
+	  }
+	  return num;	  
+  }
+  
 }
