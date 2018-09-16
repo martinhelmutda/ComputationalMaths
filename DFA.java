7ae3fbee1bd44d2a081272bd400c4e01bb45b450
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-09-16 11:39:03 -0500

    Complete, variable input

--- DFA.java
+++ DFA.java
@@ -1,26 +1,18 @@
 
+import java.util.Scanner;
 
 public class DFA {
-  public static void main(String[] args){
+  private static Scanner teclado;
+
+public static void main(String[] args){
 	  
-//	  String pattern = "abba";
-//	  String alphabet = "ab";
-//	  String cadena = "baabbabbaaba";
-	  
-	  String pattern = "zyx";
-	  String alphabet = "xyz";
-	  String cadena = "zyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyxzyx";
-	  
+
+	  teclado = new Scanner(System.in);
+	  String alphabet=teclado.nextLine();
+	  String pattern=teclado.nextLine();	 
+	  String cadena=teclado.nextLine();  
 	  System.out.println(finite_automaton_matcher(cadena, transition(pattern, alphabet), pattern.length(), alphabet));
-	  //transition(pattern, alphabet);
-	  
-	  if(pattern.contains(alphabet)) {
-		  System.out.println("Sí está");
-		  
-		
-	  }else {
-		  System.out.println("No está");
-	  }
+
   }
   
   
@@ -30,15 +22,11 @@ public class DFA {
 			
 			int m = pattern.length();
 			int [][] delta = new int[100][m];
-			char[] patternArr=pattern.toCharArray();
-			
 			String pq = null;
 			String pk= pattern.substring(0,1);
 			
 			for (int q = 0; q <= m; q++) {
-				cnum=0;
-				
-				
+				cnum=0;	
 				for(char c: alphabet.toCharArray()) {
 					k=Math.min(m+1, q+2);
 					pq=pattern.substring(0, q)+c;
@@ -55,12 +43,7 @@ public class DFA {
 				}				
 			}
 	
-//			for (int i = 0; i < m+1; i++) {
-//				for (int j = 0; j < alphabet.length(); j++) {
-//					System.out.print(delta[i][j] +" ");
-//				}
-//				System.out.println();
-//			}
+
 			
 	  return delta;	  
   }
@@ -69,14 +52,9 @@ public class DFA {
 	  int n = cadena.length();
 	  int q = 0, aux, contador=0;
 	  char [] cadenaArr= cadena.toCharArray();
-	  for (int i = 0; i < n; i++) {		
-		  
+	  for (int i = 0; i < n; i++) {				  
 		aux=numChar(cadenaArr[i],pattern);
-//		System.out.println("HOLA:" + cadenaArr[i]+" so "+ aux);
-		
 		q = matrix[q][aux];
-		
-//		System.out.println("Esta es i:"+q+"  "+aux + " " + q);
 		if (q==m) {
 //			System.out.println("Pattern occurs" + q);
 			contador++;
@@ -89,7 +67,6 @@ public class DFA {
 	  char [] alphabetArr = alphabet.toCharArray();
 	  int num=0;
 	  for (int i= 0; i < alphabetArr.length;i++) {
-//		  System.out.println(x +" estamos en este numero " +alphabetArr[i]);
 		   if(alphabetArr[i]==x) {
 			   num=i;
 		   }
@@ -98,3 +75,4 @@ public class DFA {
   }
   
 }
+
